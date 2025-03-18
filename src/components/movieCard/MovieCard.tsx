deb7dda728c07d95701883715875644d1f7ee3cd
Julia T <juliaturkali@gmail.com>
2025-03-18 15:39:44 +0100

    Refactor MovieCard component layout for improved responsiveness and styling

--- src/components/movieCard/MovieCard.tsx
+++ src/components/movieCard/MovieCard.tsx
@@ -7,16 +7,18 @@ const MovieCard = ({ movie }: { movie: IMovie }) => {
     <Link to={`/movie/${movie.title.replace(/\s+/g, "-")}`}>
       <article
         key={movie.title}
-        className="movie card card-border cursor-pointer shadow-sm"
+        className="movie card card-border min-h-60 cursor-pointer shadow-sm"
       >
-        <div className="card-body">
-          <h2 className="card-title">{movie.title}</h2>
-          <p>Release: {movie.year}</p>
-          <p>
-            Director: <span className="font-bold">{movie.director}</span>
-          </p>
-          <p>Duration: {movie.duration}</p>
-          <div className="flex flex-wrap gap-2">
+        <div className="card-body flex flex-grow flex-col justify-between">
+          <div className="flex flex-col gap-2">
+            <h2 className="card-title mb-2">{movie.title}</h2>
+            <p>Release: {movie.year}</p>
+            <p>
+              Director: <span className="font-bold">{movie.director}</span>
+            </p>
+            <p>Duration: {movie.duration}</p>
+          </div>
+          <div className="mt-4 flex flex-wrap gap-2">
             {movie.genre.map((genre) => {
               return (
                 <div
