ce9a56dfb114caa643b0bcc9b1e2496c81bd698d
Julia T <juliaturkali@gmail.com>
2025-03-18 17:43:29 +0100

    Remove unused import from Layout component for cleaner code

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Footer from "../components/footer/Footer";
 import Header from "../components/header/Header";
 
