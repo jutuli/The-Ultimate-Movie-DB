de6777b8cfbcf5713c3c806d2562374d3bd8471e
Julia T <juliaturkali@gmail.com>
2025-03-18 14:45:15 +0100

    Refactor Home component to use MovieList for rendering movie cards

---
+++ src/components/movieList/MovieList.tsx
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import MovieCard from "../movieCard/MovieCard";
+import { mainContext } from "../../context/MainProvider";
+import { IMovie } from "../../interfaces/Interfaces";
+
+const MovieList = () => {
+  const { movieList } = useContext(mainContext) as { movieList: IMovie[] };
+
+  return (
+    <div className="movie-grid grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {movieList.map((movie) => (
+        <MovieCard key={movie.title} movie={movie} />
+      ))}
+    </div>
+  );
+};
+
+export default MovieList;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,18 +1,9 @@
-import { useContext } from "react";
-import { mainContext } from "../../context/MainProvider";
-import { IMovie } from "../../interfaces/Interfaces";
-import MovieCard from "../../components/movieCard/MovieCard";
+import MovieList from "../../components/movieList/MovieList";
 
 const Home = () => {
-  const { movieList } = useContext(mainContext) as { movieList: IMovie[] };
-
   return (
     <section className="home mx-10 lg:mx-20">
-      <div className="movie-grid grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {movieList.map((movie) => (
-          <MovieCard key={movie.title} movie={movie} />
-        ))}
-      </div>
+      <MovieList />
     </section>
   );
 };
