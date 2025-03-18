c348b7a2dcedf8ef5193d299043446773c8129c8
Julia T <juliaturkali@gmail.com>
2025-03-18 18:33:35 +0100

    Enhance MovieCard component by increasing height and bolding text for release year, duration, and rating for improved readability

--- src/components/movieCard/MovieCard.tsx
+++ src/components/movieCard/MovieCard.tsx
@@ -18,17 +18,23 @@ const MovieCard = ({ movie }: { movie: IMovie }) => {
     >
       <article
         key={movie.title}
-        className="movie card card-border min-h-60 cursor-pointer shadow-sm"
+        className="movie card card-border min-h-72 cursor-pointer shadow-sm"
       >
         <div className="card-body flex flex-grow flex-col justify-between">
           <div className="flex flex-col gap-2">
             <h2 className="card-title mb-2">{movie.title}</h2>
-            <p>Release: {movie.year}</p>
+            <p>
+              Release: <span className="font-bold">{movie.year}</span>
+            </p>
             <p>
               Director: <span className="font-bold">{movie.director}</span>
             </p>
-            <p>Duration: {movie.duration}</p>
-            <p>Rating: {movie.rate} / 10</p>
+            <p>
+              Duration: <span className="font-bold">{movie.duration}</span>
+            </p>
+            <p>
+              Rating: <span className="font-bold">{movie.rate} / 10</span>
+            </p>
           </div>
           <div className="mt-4 flex flex-wrap gap-2">
             {movie.genre.map((genre) => (
